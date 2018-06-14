e1aced2ec2743d8d32ca0435e2179d2016a7a71d
Michael van den Oudenalder <michael.oudenalder@valtech.nl>
2018-06-14 11:42:44 +0200

    Fixed babel plugin issues by adding a .babelrc file.

--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -9,22 +9,23 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: [require('@babel/plugin-proposal-object-rest-spread')]
-					}
-				}
-			},
-			{
 				test: /\.css$/,
 				use: [
 					'style-loader',
 					'css-loader'
 				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						plugins: [
+							'syntax-object-rest-spread'
+						]
+					}
+				}
 			}
 		]
 	}
